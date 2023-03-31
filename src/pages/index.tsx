fb6acdc666366012aa3382c3b779666e0e39b90d
Roger <worth22focus3000@gmail.com>
2023-03-31 10:57:32 +0800

    feat: cards stack component and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,29 @@ import Tech from '@/components/Tech'
 import Feedbacks from '@/components/Feedbacks'
 import Contact from '@/components/Contact'
 
+import CardsStack from '@/components/home/CardsStack'
+import Stagger from '@/components/animation/Stagger'
+import { motion } from 'framer-motion'
+
 const StarsCanvas = dynamic(() => import('@/components/canvas/Stars'), {
   ssr: false,
 })
+
+const fadeInConfig = {
+  hidden: {
+    x: 0,
+    y: -50,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+}
 
 export default function Home() {
   return (
@@ -29,6 +49,28 @@ export default function Home() {
         </div>
         {/* <About /> */}
         <Experience />
+        {/* Work Experience */}
+        <div className="padding max-w-7xl mx-auto relative z-0 flex flex-col sm:flex-row  justify-center items-center ">
+          <div className="bg-white w-[340px] h-[340px] mb-[10px] rounded-[10px]">
+            <Stagger stagger={0.2} delay={5}>
+              <ul>
+                <motion.div variants={fadeInConfig}>
+                  <li>第一點哦 聽清楚了</li>
+                </motion.div>
+                <motion.div variants={fadeInConfig}>
+                  <li>第一點哦 聽清楚了</li>
+                </motion.div>
+                <motion.div variants={fadeInConfig}>
+                  <li>第一點哦 聽清楚了</li>
+                </motion.div>
+                <motion.div variants={fadeInConfig}>
+                  <li>第一點哦 聽清楚了</li>
+                </motion.div>
+              </ul>
+            </Stagger>
+          </div>
+          <CardsStack />
+        </div>
         <Tech />
         {/* <Works /> */}
         <Feedbacks />
