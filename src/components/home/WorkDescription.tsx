c2c890c7f16c2017f21da3bf03e8bcb79e1008a4
Roger <worth22focus3000@gmail.com>
2023-04-13 15:13:30 +0800

    feat: add work component text and change styles

--- src/components/home/WorkDescription.tsx
+++ src/components/home/WorkDescription.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion } from '@/plugins/FramerMotion'
 import Tilt from 'react-parallax-tilt'
 import { fadeIn } from '@/utils/FramerMotion'
 import { Direction, Type, Ease } from '@/types'
@@ -10,7 +10,7 @@ type ItemProps = {
 const Item = ({ index, text }: ItemProps) => {
   return (
     <motion.div
-      className="text-white text-[18px] text-start"
+      className="text-slate-500 text-[18px] text-start hover:text-white hover:transition hover:duration-300"
       variants={fadeIn(
         Direction.Down,
         Type.Spring,
@@ -19,7 +19,7 @@ const Item = ({ index, text }: ItemProps) => {
         Ease.EaseInOut
       )}
     >
-      {index}: {text}
+      {text}
     </motion.div>
   )
 }
@@ -29,9 +29,17 @@ type DisplayPanelProps = {
 }
 const DisplayPanel = ({ data }: DisplayPanelProps) => {
   return (
-    <Tilt tiltReverse={true} className="w-full h-full">
+    <Tilt
+      tiltReverse={true}
+      scale={1.1}
+      transitionSpeed={1000}
+      perspective={500}
+      tiltMaxAngleX={5}
+      tiltMaxAngleY={5}
+      className="w-full h-full select-none"
+    >
       <div className="w-full h-full green-pink-gradient p-[2px] rounded-[20px] shadow-card">
-        <div className="w-full h-full bg-tertiary rounded-[20px] p-4 flex flex-col">
+        <div className="w-full h-full bg-tertiary rounded-[20px] p-6 flex flex-col justify-between">
           {data.map((text, index) => (
             <Item key={text} index={index} text={text} />
           ))}
