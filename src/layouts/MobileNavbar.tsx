8e4245feea14021670580469d58a34efe468094a
Roger <worth22focus3000@gmail.com>
2023-04-14 09:50:48 +0800

    feat: mobile navbar navigation

--- src/layouts/MobileNavbar.tsx
+++ src/layouts/MobileNavbar.tsx
@@ -1,9 +1,10 @@
 import { useRef } from 'react'
 import { motion, useCycle } from '@/plugins/FramerMotion'
 import { useDimensions } from '@/hooks/useDimensions'
-import { Animation } from '@/constants'
-import { Routes } from '@/constants/Home'
-const { mobileNavbar } = Animation
+import { mobileNavbar } from '@/constants/Animation'
+import { Routes } from '@/constants/Routes'
+import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 
 interface PathProps {
   variants: {
@@ -24,51 +25,28 @@ const Path = (props: PathProps) => (
   />
 )
 
-type ToggleFunction = () => void
-interface Toggle {
-  toggle: ToggleFunction
-}
-const MenuToggle = ({ toggle }: Toggle) => (
-  <button
-    onClick={toggle}
-    className="absolute top-5 right-[25.5px] w-[50px] h-[50px] flex justify-center items-center rounded-full bg-transparent cursor-pointer"
-  >
-    <svg width="23" height="23" viewBox="0 0 23 23">
-      <Path variants={mobileNavbar.buttonVariants.topLine} />
-      <Path
-        d="M 2 9.423 L 20 9.423"
-        variants={mobileNavbar.buttonVariants.centerLine}
-        transition={{ duration: 0.1 }}
-      />
-      <Path variants={mobileNavbar.buttonVariants.bottomLine} />
-    </svg>
-  </button>
-)
+type MenuItemProps = { name: string; path: string; toggleOpen: () => void }
 
-type MenuItemProps = { i: string }
-const MenuItem = ({ i }: MenuItemProps) => {
+const MenuItem = ({ name, path, toggleOpen }: MenuItemProps) => {
+  const { t } = useTranslation('Home')
+  const router = useRouter()
+  const handleClick = () => {
+    router.push(path)
+    toggleOpen()
+  }
+
   return (
     <motion.li
       className="list-none cursor-pointer text-white text-xl"
       variants={mobileNavbar.liVariants}
       whileHover={{ scale: 1.3 }}
       whileTap={{ scale: 0.8 }}
+      onClick={handleClick}
     >
-      {i}
+      {t(name)}
     </motion.li>
   )
 }
-
-const Navigation = () => (
-  <motion.ul
-    variants={mobileNavbar.ulVariants}
-    className="absolute w-full h-full top-0 left-0 flex flex-col gap-10 justify-center items-center"
-  >
-    {Routes.map((i) => (
-      <MenuItem i={i.name} key={i.name} />
-    ))}
-  </motion.ul>
-)
 
 const MobileNavbar: React.FC = () => {
   const [isOpen, toggleOpen] = useCycle(false, true)
@@ -78,7 +56,7 @@ const MobileNavbar: React.FC = () => {
   return (
     <motion.nav
       className={`fixed top-0 right-0 w-full h-full ${
-        isOpen ? 'z-20' : 'z-10'
+        isOpen ? 'z-30' : 'z-20'
       } overflow-hidden`}
       initial={false}
       animate={isOpen ? 'open' : 'closed'}
@@ -86,11 +64,37 @@ const MobileNavbar: React.FC = () => {
       ref={containerRef}
     >
       <motion.div
-        className="absolute top-0 right-0 w-[100vw] h-[100vh] bg-indigo-900"
+        className={`${
+          isOpen ? 'z-10' : 'z-0'
+        } absolute top-0 right-0 w-[100vw] h-[100vh] bg-indigo-900`}
         variants={mobileNavbar.circleVariants}
       />
-      <Navigation />
-      <MenuToggle toggle={toggleOpen} />
+      <motion.ul
+        variants={mobileNavbar.ulVariants}
+        className={`${
+          isOpen ? 'z-10' : 'z-0'
+        } absolute w-full h-full top-0 left-0 flex flex-col gap-10 justify-center items-center`}
+      >
+        {Routes.map((i) => (
+          <MenuItem {...i} key={i.name} toggleOpen={toggleOpen} />
+        ))}
+      </motion.ul>
+      <button
+        onClick={() => toggleOpen()}
+        className={`${
+          isOpen ? 'z-30' : 'z-20'
+        } absolute top-5 right-[25.5px] w-[50px] h-[50px] flex justify-center items-center rounded-full bg-transparent cursor-pointer`}
+      >
+        <svg width="23" height="23" viewBox="0 0 23 23">
+          <Path variants={mobileNavbar.buttonVariants.topLine} />
+          <Path
+            d="M 2 9.423 L 20 9.423"
+            variants={mobileNavbar.buttonVariants.centerLine}
+            transition={{ duration: 0.1 }}
+          />
+          <Path variants={mobileNavbar.buttonVariants.bottomLine} />
+        </svg>
+      </button>
     </motion.nav>
   )
 }
