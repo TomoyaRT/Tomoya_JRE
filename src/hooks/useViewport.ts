2a927c177dcfeeb5721b69a7b215e2454dc23544
Roger <worth22focus3000@gmail.com>
2023-04-14 09:52:02 +0800

    fix: server error and initial value

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -1,16 +1,17 @@
 import { useState, useLayoutEffect, useCallback } from 'react'
-import { debounce as _debounce } from 'lodash-es'
+import { debounce } from '@/plugins/Lodash'
 import { ViewportSize } from '@/types'
 
 function useViewport(): ViewportSize {
-  const [width, setWidth] = useState(window.innerWidth)
-  const [height, setHeight] = useState(window.innerHeight)
+  const isClient = typeof window === 'object'
+  const [width, setWidth] = useState(isClient ? window.innerWidth : 0)
+  const [height, setHeight] = useState(isClient ? window.innerHeight : 0)
 
   const handleResize = useCallback(
-    _debounce(() => {
+    debounce(() => {
       setWidth(window.innerWidth)
       setHeight(window.innerHeight)
-    }, 200),
+    }, 100),
     []
   )
 
