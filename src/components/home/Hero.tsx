6beec7b1192a911a2512e95aa79ae62018ef5a10
Roger <worth22focus3000@gmail.com>
2023-04-15 22:17:18 +0800

    refactor: Hero component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,8 +1,6 @@
 import dynamic from 'next/dynamic'
-import GradientLine from '@/components/home/GradientLine'
-import HeroText from '@/components/home/HeroText'
 import ScrollDownButton from '@/components/home/ScrollDownButton'
-
+import HeroDescription from '@/components/home/HeroDescription'
 const ComputersCanvas = dynamic(() => import('@/components/canvas/Computer'), {
   ssr: false,
 })
@@ -11,8 +9,7 @@ const Hero: React.FC = () => {
   return (
     <div className="bg-home-pattern bg-cover bg-no-repeat bg-center">
       <section className="relative w-full h-screen mx-auto">
-        <GradientLine />
-        <HeroText />
+        <HeroDescription />
         <ComputersCanvas />
         <ScrollDownButton />
       </section>
---
+++ src/components/home/HeroDescription.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from 'next-i18next'
+import { Hero } from '@/constants/Home'
+import ParticlesText from '@/components/canvas/ParticlesText'
+
+const HeroDescription = () => {
+  const { t } = useTranslation('Home')
+
+  return (
+    <div
+      className={
+        'w-full absolute z-10 pl-[17%] pt-28 text-white-100 select-none flex flex-row justify-start'
+      }
+    >
+      <div className="flex flex-col justify-center items-center">
+        <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+        <div className="w-1 sm:h-72 h-40 violet-gradient"></div>
+      </div>
+      <div className="relative top-0 left-5 h-full">
+        <div className="font-black text-white text-[50px] flex flex-row items-center">
+          <div>{t(Hero[0])}</div>
+          <ParticlesText text={t(Hero[1])} color={'#7737ff'} font={'Arial'} />
+        </div>
+        <p className="font-medium text-white text-[20px]">
+          {t(Hero[2])}
+          <br />
+          {t(Hero[3])}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroDescription
