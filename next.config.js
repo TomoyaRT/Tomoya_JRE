d205b1c66fd6745c584ca771de7171f377411774
Roger <worth22focus3000@gmail.com>
2023-04-20 11:20:55 +0800

    feat: add bundle-analyzer, use compressed .glb

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const { i18n } = require('./next-i18next.config.js')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
+  productionBrowserSourceMaps: true,
   i18n,
   reactStrictMode: true,
-}
+})
 
 module.exports = nextConfig
