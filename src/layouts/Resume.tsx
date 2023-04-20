7a8699dfe52d76b742559a3f6bb2f7ebabd4db9f
Roger <worth22focus3000@gmail.com>
2023-04-20 22:25:59 +0800

    fix: resume download

--- src/layouts/Resume.tsx
+++ src/layouts/Resume.tsx
@@ -1,8 +1,9 @@
 import styles from '@/styles/modules/Resume.module.css'
+import { Resume as ResumePath } from '@/constants/Public'
 
 const Resume = () => {
   return (
-    <a href="/resume/Roger.pdf" className={styles.resume} download>
+    <a href={ResumePath} className={styles.resume} download>
       <span>Resume</span>
       <i></i>
     </a>
