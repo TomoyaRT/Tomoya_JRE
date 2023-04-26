9dfea48f13598f2a8e75e054d3dd067a75ac26ae
Roger <worth22focus3000@gmail.com>
2023-04-26 10:44:53 +0800

    refactor: data key rename

--- src/components/home/WorkExperience.tsx
+++ src/components/home/WorkExperience.tsx
@@ -8,18 +8,18 @@ import { WorkExperience as WorkExperienceText } from '@/constants/Home'
 const ComputersCanvas = dynamic(() => import('@/components/canvas/Computer'), {
   ssr: false,
 })
-const { title, subTitle, data } = WorkExperienceText
+const { title, subTitle, description } = WorkExperienceText
 
 const WorkExperience = () => {
   const { t } = useTranslation('Home')
-  const description = data.description.map((i) => t(i))
+  const descriptionData = description.map((i) => t(i))
 
   return (
     <div className="flex flex-col">
       <SectionHead title={t(title)} subTitle={t(subTitle)} styleType="hero" />
       <div className="w-full h-full mt-10 flex flex-col xl:flex-row gap-10 items-center">
         <div className="w-full md:w-[75%] lg:w-[60%] xl:w-[80%] h-[500px]">
-          <WorkDescription data={description} />
+          <WorkDescription data={descriptionData} />
         </div>
         <div className="w-full h-[400px] sm:h-[500px]">
           <ComputersCanvas />
