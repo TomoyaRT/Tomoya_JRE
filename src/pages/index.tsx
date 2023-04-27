21a6f54c7bdbb672782229caa3c999988cea89dc
Roger <worth22focus3000@gmail.com>
2023-04-27 15:36:38 +0800

    feat: change dynamic import and hidden work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import dynamic from 'next/dynamic'
 
 import LazyWrapper from '@/components/LazyWrapper'
-import Hero from '@/components/home/Hero'
 import About from '@/components/home/About'
-const Portfolio = dynamic(() => import('@/components/home/Portfolio'))
-const Journey = dynamic(() => import('@/components/home/Journey'))
+import Portfolio from '@/components/home/Portfolio'
+import Journey from '@/components/home/Journey'
 // const WorkExperience = dynamic(() => import('@/components/home/WorkExperience'))
-const BookClub = dynamic(() => import('@/components/home/BookClub'))
+import BookClub from '@/components/home/BookClub'
+const Firework = dynamic(() => import('@/p5/Firework'), {
+  ssr: false,
+})
 const Contact = dynamic(() => import('@/components/home/Contact'))
 const StarsCanvas = dynamic(() => import('@/components/canvas/Stars'), {
   ssr: false,
@@ -17,10 +19,13 @@ const StarsCanvas = dynamic(() => import('@/components/canvas/Stars'), {
 const Home: React.FC = () => {
   return (
     <div className="relative z-0">
-      <Hero />
+      <section className="w-full h-screen">
+        <Firework />
+      </section>
       <About />
       <Portfolio />
       <Journey />
+      {/* <WorkExperience /> */}
       <BookClub />
       <LazyWrapper>
         <div className="relative z-0">
